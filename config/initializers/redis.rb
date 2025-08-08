efcec5eb6e19ac12918f84e1ef65cc7d0b93a7b0
nerdawey <nerdawy@icloud.com>
2025-08-08 18:58:19 +0300

    Fix Sidekiq ERB configuration issue by removing ERB from sidekiq.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,19 +1,17 @@
-# Redis configuration for production
+# Redis configuration for all environments
 Rails.application.config.after_initialize do
-  if Rails.env.production?
-    begin
-      redis_url = ENV['REDIS_URL']
-      if redis_url
-        $redis = Redis.new(url: redis_url)
-        # Test the connection
-        $redis.ping
-        Rails.logger&.info "Redis connected successfully"
-      else
-        Rails.logger&.warn "REDIS_URL not set, Redis features will be disabled"
-      end
-    rescue => e
-      Rails.logger&.error "Redis connection failed: #{e.message}"
-      # Don't crash the app if Redis is not available
+  begin
+    redis_url = ENV['REDIS_URL']
+    if redis_url
+      $redis = Redis.new(url: redis_url)
+      # Test the connection
+      $redis.ping
+      Rails.logger&.info "Redis connected successfully: #{redis_url}"
+    else
+      Rails.logger&.warn "REDIS_URL not set, Redis features will be disabled"
     end
+  rescue => e
+    Rails.logger&.error "Redis connection failed: #{e.message}"
+    # Don't crash the app if Redis is not available
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,24 @@
-# Configure Sidekiq for production
+# Configure Sidekiq for all environments
 Rails.application.config.after_initialize do
-  if Rails.env.production?
-    redis_url = ENV['REDIS_URL']
-    
-    if redis_url
-      Sidekiq.configure_server do |config|
-        config.redis = { url: redis_url }
-      end
-
-      Sidekiq.configure_client do |config|
-        config.redis = { url: redis_url }
-      end
-      
-      Rails.logger&.info "Sidekiq configured with Redis"
-    else
-      Rails.logger&.warn "REDIS_URL not set, Sidekiq will not work properly"
+  redis_url = ENV['REDIS_URL']
+  
+  if redis_url
+    Sidekiq.configure_server do |config|
+      config.redis = { 
+        url: redis_url,
+        namespace: "search_analytics_#{Rails.env}"
+      }
     end
+
+    Sidekiq.configure_client do |config|
+      config.redis = { 
+        url: redis_url,
+        namespace: "search_analytics_#{Rails.env}"
+      }
+    end
+    
+    Rails.logger&.info "Sidekiq configured with Redis: #{redis_url}"
+  else
+    Rails.logger&.warn "REDIS_URL not set, Sidekiq will use default configuration"
   end
 end
